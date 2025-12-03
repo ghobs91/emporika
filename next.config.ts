ba38706c12d31486b77b8fa3a42985aed4dc10ab
Andrew Ghobrial <andrew@ghobrial.me>
2025-12-03 04:46:37 -0500

    added best buy

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: '*.walmartimages.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'pisces.bbystatic.com',
+      },
     ],
   },
 };
